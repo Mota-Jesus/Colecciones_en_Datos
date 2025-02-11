d8aadd7f832cf7b4f25b0bdb38b773c2861a8b35
Mota-Jesus <a.22308051281275@cbtis128.edu.mx>
2025-02-11 16:53:55 +0000

    hola lista

---
+++ lista1.dart
@@ -0,0 +1,15 @@
+void main() {  
+  // lista tipo entero de 3 elementos
+  List<int> numeros = [10, 20, 30];
+  print("Lista de numeros enteros: ");
+  print(numeros); 
+  print("Primer elemento: ${numeros[0]}");
+  print("Segundo elemento: ${numeros[1]}");
+  print("Tercer elemento: ${numeros[2]} \n");
+  print("Elementos de la lista usando ciclo for: ");
+  for (int i = 0; i < numeros.length; i++) {
+    print(numeros[i]);
+  }
+
+
+}  
